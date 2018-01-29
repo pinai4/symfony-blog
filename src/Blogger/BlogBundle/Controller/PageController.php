5dbebd309e77d112ab514133858be2ff74896e88
Sergey P <pinai4@gmail.com>
2018-01-30 02:35:45 +0300

    Changes (Create demo blog. Manual page 2 part 2)

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -29,18 +29,30 @@ class PageController extends Controller
 
         if ($request->isMethod($request::METHOD_POST)) {
             $form->handleRequest($request);
-
             if ($form->isValid()) {
-                // Perform some action, such as sending an email
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+
+
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
 
                 // Redirect - This is important to prevent users re-posting
                 // the form if they refresh the page
                 return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+
             }
+
         }
 
         return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
             'form' => $form->createView()
         ));
+
     }
+
 }
