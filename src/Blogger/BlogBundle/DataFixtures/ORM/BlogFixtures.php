131a158ddad576edb1a6ec72d25f535ae61dc85c
Sergey P <pinai4@gmail.com>
2018-02-02 00:23:08 +0300

    Changes (Create demo blog. Manual page 3 part 2)

--- src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
+++ src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
@@ -17,8 +17,6 @@ class BlogFixtures implements FixtureInterface
         $blog1->setImage('beach.jpg');
         $blog1->setAuthor('dsyph3r');
         $blog1->setTags('symfony2, php, paradise, symblog');
-        $blog1->setCreated(new \DateTime());
-        $blog1->setUpdated($blog1->getCreated());
         $manager->persist($blog1);
 
         $blog2 = new Blog();
@@ -27,8 +25,6 @@ class BlogFixtures implements FixtureInterface
         $blog2->setImage('pool_leak.jpg');
         $blog2->setAuthor('Zero Cool');
         $blog2->setTags('pool, leaky, hacked, movie, hacking, symblog');
-        $blog2->setCreated(new \DateTime("2011-07-23 06:12:33"));
-        $blog2->setUpdated($blog2->getCreated());
         $manager->persist($blog2);
 
         $blog3 = new Blog();
@@ -37,8 +33,6 @@ class BlogFixtures implements FixtureInterface
         $blog3->setImage('misdirection.jpg');
         $blog3->setAuthor('Gabriel');
         $blog3->setTags('misdirection, magic, movie, hacking, symblog');
-        $blog3->setCreated(new \DateTime("2011-07-16 16:14:06"));
-        $blog3->setUpdated($blog3->getCreated());
         $manager->persist($blog3);
 
         $blog4 = new Blog();
@@ -47,8 +41,6 @@ class BlogFixtures implements FixtureInterface
         $blog4->setImage('the_grid.jpg');
         $blog4->setAuthor('Kevin Flynn');
         $blog4->setTags('grid, daftpunk, movie, symblog');
-        $blog4->setCreated(new \DateTime("2011-06-02 18:54:12"));
-        $blog4->setUpdated($blog4->getCreated());
         $manager->persist($blog4);
 
         $blog5 = new Blog();
@@ -57,8 +49,6 @@ class BlogFixtures implements FixtureInterface
         $blog5->setImage('one_or_zero.jpg');
         $blog5->setAuthor('Gary Winston');
         $blog5->setTags('binary, one, zero, alive, dead, !trusting, movie, symblog');
-        $blog5->setCreated(new \DateTime("2011-04-25 15:34:18"));
-        $blog5->setUpdated($blog5->getCreated());
         $manager->persist($blog5);
 
         $manager->flush();
